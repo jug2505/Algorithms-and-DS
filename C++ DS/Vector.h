06c840ce3797f32065f009c49f2ee90c02b5037e
Evgeny Kuvshinov <41694637+jug2505@users.noreply.github.com>
2019-12-19 17:27:09 +0400

    Update Vector.h

--- C++ DS/Vector.h
+++ C++ DS/Vector.h
@@ -14,7 +14,18 @@ namespace NoSTD{
       // Default constructor
       Vector();
       explicit Vector(std::size_t elems);
-
+      // Copy constructor
+      Vector(const Vector&);
+      // Copy assignment
+      Vector<T>& operator=(const Vector<T>&);
+      // Destructor
+      ~Vector() {delete[] data};
+      // Iterators
+      iterator begin(){return data};
+      const iterator begin() const {return data};
+      iterator end(){return data + size};
+      const iterator end() const {return data + size};
+    
     private:
       // Pointer to the first element of data array
       T* data;
@@ -35,12 +46,30 @@ NoSTD::Vector<T>::Vector(){
 }
 
 template<class T> 
-NoSTD::Vector<T>::Vector(std::size_t elems){
-  if(size = elems; size > defaultCapacity)
-    capacity = elems * 2;
-  else
-    capacity = defaultCapacity;
+NoSTD::Vector<T>::Vector(std::size_t elems):size(elems){
+  capacity = size > defaultCapacity ? elems * 2 : defaultCapacity;
   for(size_t i = 0; i < size; ++i)
     data[i] = T();
 }
+
+template<class T>
+NoSTD::Vector<T>::Vector(const Vector& arg):size(arg.size), capacity(arg.capacity), data(new T[capacity]){
+  for(size_t i = 0; i < arg.size; ++i)
+    data[i] = arg.data[i];
+}  
+
+template<class T>
+NoSTD::Vector<T>::Vector<T>& operator=(const Vector<T>& arg){
+  if (this == &arg) return *this; // Self assignment
+  if (capacity < arg.size){
+    delete[] data;
+    capacity = arg.capacity;
+    data = new T[capacity];
+  }
+  size = arg.size;
+  for(size_t i = 0; i < arg.size; ++i)
+      data[i] = arg.data[i];
+  return *this;
+}
+
 #endif
