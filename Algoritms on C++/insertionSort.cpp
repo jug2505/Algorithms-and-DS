53948e9a3b42793ff3d87307e1da7cd8817f9659
Evgeny Kuvshinov <41694637+jug2505@users.noreply.github.com>
2020-01-30 09:43:47 +0400

    Update insertionSort.cpp

--- Algoritms on C++/insertionSort.cpp
+++ Algoritms on C++/insertionSort.cpp
@@ -1,14 +1,10 @@
 void insertionSort(int* arr, int n){
-    int key;
     int i, j;
-    for (j = 1; j < n; ++j){
-        key = arr[j];
-        // Insert arr[j] into the sorted sequence
-        i = j - 1;
-        while (i >= 0 && arr[i] > key){
-            arr[i + 1] = arr [i];
-            i = i - 1;
+    for (i = 1; i < n; ++i){
+        j = i;
+        while (j > 0 && arr[j] < arr[j-1]){
+            std::swap(&arr[j], &arr[j-1]);
+            j = j - 1;
         }
-        arr[i + 1] = key;
     }
 }
